6eb5da5b7ac4d691badd317bb1db3e04792cb01e
tonymbarton <36773585+tonymbarton@users.noreply.github.com>
2019-02-25 13:58:55 -0500

    Add files via upload

---
+++ ProxyAddresses.ps1
@@ -0,0 +1,10 @@
+﻿Import-Module ActiveDirectory
+
+$domain = "@domainname.com"
+$users = Get-ADUser -Filter {ProxyAddresses -notlike "*"} -Properties SamAccountName, ProxyAddresses
+
+Foreach ($user in $users) {
+
+    Set-ADUser -Identity $user.samaccountname -Add @{Proxyaddresses="SMTP:"+$user.samaccountname+$domain}
+
+    }
